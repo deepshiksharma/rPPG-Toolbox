b54a0db5992eeebd49806f7ddd6d5268200a319e
deepshiksharma <definitelynotdeepshik@gmail.com>
2025-02-06 15:43:02 +0530

    minor changes to .gitignore, readme, yaml config file, requirements, and setup_updated

--- setup_updated.sh
+++ setup_updated.sh
@@ -2,5 +2,5 @@ conda create -n rppg-toolbox python=3.8 -y
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate rppg-toolbox
 
-pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 torchaudio==2.1.2+cu121 --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
+pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 torchaudio==2.1.2+cu121 --index-url https://download.pytorch.org/whl/cu121
